ec39943980594c3a8923c1cf903e55ee6c5d2fe3
EricCheeley <70220147+EricCheeley@users.noreply.github.com>
2023-10-01 22:02:45 -0500

    Update Client.h

--- Client.h
+++ Client.h
@@ -1,12 +1,39 @@
-/*
-In this file you must create your class and prototype your public functions.
-*/
+#ifndef CLIENT_H
+#define CLIENT_H
 
-class Client
-{ 
-    private:
-        //define your private members here
+#include <string>
 
-    public:
-        //define your public members here
+// Define the Client struct
+struct Client {
+    std::string client_name;
+    std::string ssn;
+    double bank_account;
+    double balance;
 };
+
+// Define the Bank class
+class Bank {
+private:
+    int num_clients;
+    Client* client_info;
+
+public:
+    // Constructor
+    Bank(const std::string& filename);
+
+    // Deep Copy Constructor
+    Bank(const Bank& other);
+
+    // Destructor
+    ~Bank();
+
+    // Public member functions
+    void set_client_info(const Client* clients, int num_clients);
+    Client* get_client_info();
+    double deposit(int account_number, double amount);
+    std::string withdraw(int account_number, double amount);
+    void saving_info(const std::string& filename);
+    void find_client(int account_number);
+};
+
+#endif // CLIENT_H
